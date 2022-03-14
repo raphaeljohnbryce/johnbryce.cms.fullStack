5c0835112ed2af437166a6d9f7999d76501c8b0e
RaphaelArtlist <raphael.b@artlist.io>
2022-03-14 21:42:01 +0200

    setup client side

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import  express from "express";
 import { customersRouter } from "./controllers/customers-controller.js";
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api', customersRouter)
 
