40107ca845505b8a7c456f47d1424913b5d5b1b9
RaphaelArtlist <raphael.b@artlist.io>
2022-03-14 20:40:44 +0200

    4

---
+++ server/dal/customers-dal.js
@@ -0,0 +1,20 @@
+// import connection from '../db.js';
+
+// connection.query(
+//     'SELECT * FROM customers',
+//     (err, results) => {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log(results)
+//         }
+//     }
+// )
+
+// const getAll = () => {
+
+// }
+
+// export {
+//     getAll
+// }
