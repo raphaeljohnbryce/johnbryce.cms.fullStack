9c369c7d67cf8d43d119b3f2d777b22b1e4f9e38
RaphaelArtlist <raphael.b@artlist.io>
2022-03-14 20:07:38 +0200

    init server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import mysql from 'mysql2';
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Aa123456!',
+    database: 'cms'
+})
+
+connection.query(
+    'SELECT * FROM customers',
+    (err, results) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(results)
+        }
+    }
+)
