ece80171dcaf6198c4ebb74805a49607e3c775dd
RaphaelArtlist <raphael.b@artlist.io>
2022-03-14 20:09:16 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Aa123456!',
+    password: 'Aa123456',
     database: 'cms'
 })
 
