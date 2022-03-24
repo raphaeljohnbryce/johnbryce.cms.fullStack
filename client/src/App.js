a9adf9ab2b411022232b304a33d0c17e87651169
Raphael <you@example.com>
2022-03-24 20:49:25 +0200

    client side

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,46 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [customers, setCustomers] = useState([]);
+    const [customers, setCustomers] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/customers')
-      .then(res => res.json())
-      .then(data => {
-        setCustomers(data);
-      })
-  }, []);
+    useEffect(() => {
+        fetch('http://localhost:5000/api/customers')
+            .then(res => res.json())
+            .then(data => {
+                setCustomers(data.data);
+            })
+    }, []);
 
-  return (
-    <div className="App">
-      <h1>Customer Management System</h1>
-      <ul>
-        {customers.map(customer => (
-          <li key={customer.id}>
-            {customer.firstName} {customer.lastName}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    const handleDeleteCustomer = (id) => {
+        fetch('http://localhost:5000/api/customers/' + id,
+            { method: 'DELETE' })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+
+                let tempCustomers = []
+
+                tempCustomers = customers.filter(c => c.id !== id)
+                setCustomers(tempCustomers);
+            })
+    }
+
+    return (
+        <div className="App">
+            <h1>Customer Management System</h1>
+            <ul>
+                {customers.map(customer => (
+                    <li
+                        onClick={() => {
+                            handleDeleteCustomer(customer.id)
+                        }}
+                        key={customer.id}>
+                        {customer.firstName} {customer.lastName}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
